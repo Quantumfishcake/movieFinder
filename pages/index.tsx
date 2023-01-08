5da680cfb4d44525cdb46ec66cfb9fff6205ac6e
Tom Quant <tom.quant@longtailux.com.au>
2023-01-08 17:31:07 +1100

    removed serversideprops function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import App from "./main"
 
-export function getServerSideProps() {
-  const renderedAt = new Date();
-  const formattedBuildDate = renderedAt.toLocaleDateString("en-US", {
-    dateStyle: "long",
-  });
-  const formattedBuildTime = renderedAt.toLocaleTimeString("en-US", {
-    timeStyle: "long",
-  });
-  return {
-    props: {
-      renderedAt: `${formattedBuildDate} at ${formattedBuildTime}`,
-    },
-  };
-}
+// export function getServerSideProps() {
+//   const renderedAt = new Date();
+//   const formattedBuildDate = renderedAt.toLocaleDateString("en-US", {
+//     dateStyle: "long",
+//   });
+//   const formattedBuildTime = renderedAt.toLocaleTimeString("en-US", {
+//     timeStyle: "long",
+//   });
+//   return {
+//     props: {
+//       renderedAt: `${formattedBuildDate} at ${formattedBuildTime}`,
+//     },
+//   };
+// }
 
 function Home({ signOut, user, renderedAt }:any) {
   return (
