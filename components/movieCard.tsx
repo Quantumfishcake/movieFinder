f815d2362d2f2a18a51b5c62fe978c9c8441cccd
Tom Quant <tom.quant@longtailux.com.au>
2023-01-12 22:06:56 +1100

    added fallback image

--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, SyntheticEvent } from 'react';
 import { Movie } from "../types/mainTypes"
 
 
@@ -13,11 +13,17 @@ const MovieCard: FunctionComponent<MovieProps> = ({ movie, favourited, updateMov
     const image_path = `https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`
     const movie_path = `https://www.themoviedb.org/movie/${movie?.id}`
 
+    // SyntheticEvent<HTMLImageElement, Event> src doesnt exist on type
+    const replaceImage = (event: any) => {
+        //replacement of broken Image
+        event.target.src = "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"; 
+    }
+
     return (
         <>
             <div className="card w-60 bg-base-100 shadow-xl m-2">
-                <figure className="relative">
-                    <img src={image_path} alt="Shoes" />
+                <figure className="relative" style={{maxHeight: "135px"}}>
+                    <img src={image_path} onError={replaceImage} alt="Shoes" />
                     <div className="absolute bottom-2 right-2 w-14 h-14 flex bg-white flex-wrap rounded-xl border border-black">
                         <div className="text-2xl w-full flex items-center justify-center">{movie?.vote_average}</div>
                         <div className="text-sm w-full flex items-center justify-center">{movie?.vote_count}</div>
